a061fdc509b449bebad013459e81e39436cde982
Bruno  Michaelis <brunomichaelis@Brunos-MacBook-Pro.local>
2021-06-09 21:09:52 -0500

    37 - Pokedex

---
+++ 37 - Pokedex/scripts.js
@@ -0,0 +1,61 @@
+const pokeContainer = document.getElementById('poke-container')
+const pokemon_count = 150
+const colors = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#F5F5F5',
+    fighting: '#E6E0D4',
+    normal: '#F5F5F5'
+}
+
+const main_type = Object.keys(colors)
+
+const fetchPokemons = async () => {
+    for (let i = 1; i <= 150; i++) {
+        await getPokemon(i)
+    }
+}
+
+const getPokemon = async (id) => {
+    const URL = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(URL)
+    const data = await res.json()
+    createPokemonCard(data)
+}
+
+const createPokemonCard = (pokemon) => {
+    const pokemonEl = document.createElement('div')
+    pokemonEl.classList.add('pokemon')
+
+
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1, pokemon.name.lenght)
+    const id = pokemon.id.toString().padStart(3, '0')
+    const poke_types = pokemon.types.map(type => type.type.name)
+    const type = main_type.find(type => poke_types.indexOf(type) > -1)
+
+    pokemonEl.style.backgroundColor = colors[type]
+
+    const pokemonInnerHTML = `
+    <div class="img-container">
+        <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png">
+    </div>
+    <div class="info">
+        <span class="number">#${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">Type: <span>${type}</span></small> </div>`
+
+    pokemonEl.innerHTML = pokemonInnerHTML
+
+    pokeContainer.appendChild(pokemonEl)
+}
+
+fetchPokemons()
